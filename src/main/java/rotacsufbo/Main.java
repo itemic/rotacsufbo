408098d2c8675693676a5ef82c14639d9430f0f9
Terran <terrankroft@gmail.com>
2018-04-28 09:51:49 +1200

    Revert "Revert "Readded launch""
    This reverts commit 5e1738971299dd2dc7186b345124f0c08d5fc970.

--- src/main/java/rotacsufbo/Main.java
+++ src/main/java/rotacsufbo/Main.java
@@ -21,16 +21,8 @@ public class Main extends Application{
     private static BorderPane rootLayout;
 
     public static void main(String[] args) {
-        File f = new File("C:\\Users\\Terran\\IdeaProjects\\Bepis\\src\\Main.java");
-        try {
-            CompilationUnit c = JavaParser.parse(f);
-            Obfuscator o = new Obfuscator(c);
-            o.flatten();
-//            System.out.println(o.getUnit());
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-//        launch(args);
+
+        launch(args);
     }
 
 
